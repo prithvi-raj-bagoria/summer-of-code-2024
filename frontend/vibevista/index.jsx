d84e8edfe821477f883fa48e4432608748fff721
Prithvi Raj <prithvirajiitjee@gmail.com>
2024-07-12 17:47:42 +0530

    React Setup

---
+++ frontend/vibevista/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./src/Pages/Login.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Login />
+    </React.StrictMode>
+);

a9687b23f9be62b478a9c93879729de6be5f4503
Prithvi Raj <prithvirajiitjee@gmail.com>
2024-07-31 20:17:05 +0530

    BeforeLogin

---
+++ frontend/vibevista/src/Pages/Cashier/AfterLogin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AfterLogin = () => {
+  return <div>AfterLogin</div>;
+};
+
+export default AfterLogin;
--- frontend/vibevista/src/Pages/Cashier/BeforeLogin.jsx
+++ frontend/vibevista/src/Pages/Cashier/BeforeLogin.jsx
@@ -12,17 +12,19 @@ const BeforeLogin = () => {
   const [address, setAddress] = useState("");
 
   const emptyCase = () => (
-    <p className="fs-4 text-secondary">Please Enter your Details to see .</p>
+    <p className="fs-5 text-center text-danger">
+      Enter customer details to proceed to checkout
+    </p>
   );
+
+  const isAllEmpty = name === "" && mobile === "" && address === "";
+
   return (
     <>
-      <div className="m-0 p-0 container-fluid">
+      <div className="m-0 p-0 container-fluid ">
         <Navbar logo={VibeVista} logowidth="30px" bgColor="orange" />
-        <div
-          className="m-1 row d-flex flex-column flex-md-row "
-          style={{ height: "90vh" }}
-        >
-          <div className="col-3 d-flex flex-column align-items-center justify-content-center ">
+        <div className="my-5 mx-2 row d-flex flex-column flex-md-row  justify-content-evenly align-items-center">
+          <div className="col-md-3  d-flex flex-column align-items-center justify-content-center ">
             <FaCircle size="150px" color="brown" />
             <p className="mb-4 fw-light fs-3">Your Profile</p>
             <h1 className="display-4">Name</h1>
@@ -30,7 +32,10 @@ const BeforeLogin = () => {
             <Button style="btn btn-lg btn-danger rounded-5" text="CASHIER" />
             <p className="mt-3">{time.time}</p>
           </div>
-          <div className="m-1 mx-3 col-4 row d-flex flex-column align-items-center shadow-lg rounded-5 ">
+          <div
+            className=" col-md-4 row d-flex flex-column align-items-center   rounded-5 "
+            style={{ boxShadow: "0px -32px 128px rgb(255,219,187)" }}
+          >
             <h1 className="m-2  text-warning text-decoration-underline">
               Customer Details
             </h1>
@@ -80,36 +85,43 @@ const BeforeLogin = () => {
             </form>
             <p className="fw-light"> * marked fields are mandatory.</p>
           </div>
-          <div className=" ms-5 col-4 row d-flex flex-column justify-content-evenly bg-warning rounded-4">
-            {name == "" && mobile === "" && address === "" ? emptyCase() : ""}
-            <h3 className="m-2  text-center text-danger text-decoration-underline">
-              Details Entered
-            </h3>
-            <span>
-              <h2 className="text-danger">Name</h2>
-              <p className="fs-4 text-light text-center bg-danger rounded-5 ">
-                {name}
-              </p>
-            </span>
-            <span>
-              <h2 className="text-danger">Mobile</h2>
-              <p className="fs-4 text-light text-center bg-danger rounded-5 ">
-                {mobile}
-              </p>
-            </span>
-            <span>
-              <h2 className="text-danger">Address</h2>
-              <p className="fs-4 text-light text-center bg-danger rounded-5 ">
-                {address}
-              </p>
-            </span>
-
-            <div className="d-flex justify-content-end">
-              <Button
-                style=" mt-4 btn  btn-danger text-end rounded-2"
-                text="Proceed to Checkout "
-              />
-            </div>
+          <div
+            className=" row py-2 col-md-4  d-flex flex-column justify-content-evenly bg-warning rounded-4"
+            style={{ minHeight: "500px" }}
+          >
+            {isAllEmpty ? (
+              emptyCase()
+            ) : (
+              <>
+                <h3 className="m-2  text-center text-danger text-decoration-underline">
+                  Details Entered
+                </h3>
+                <span>
+                  <h2 className="text-danger">Name</h2>
+                  <p className="fs-4 p-2 text-light text-center bg-danger rounded-4 ">
+                    {name}
+                  </p>
+                </span>
+                <span>
+                  <h2 className="text-danger">Mobile</h2>
+                  <p className="fs-4 p-2 text-light text-center bg-danger rounded-4 ">
+                    {mobile}
+                  </p>
+                </span>
+                <span>
+                  <h2 className="text-danger">Address</h2>
+                  <p className="fs-4 p-2 text-light text-center bg-danger rounded-4 ">
+                    {address}
+                  </p>
+                </span>
+                <div className="d-flex justify-content-end align-items-center rounded-3">
+                  <Button
+                    style=" mt-4 btn  btn-danger text-end rounded-4"
+                    text="Proceed to Checkout "
+                  />
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
